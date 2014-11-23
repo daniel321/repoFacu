ebd27cf74bd55daf6d0f430c919d0e32b96fcf21
BNeutral <bneutral@bneutral-desktop.(none)>
2014-11-23 04:08:24 -0300

    Error mas bonito

--- tp/DatabaseMangement/cliente/main.cpp
+++ tp/DatabaseMangement/cliente/main.cpp
@@ -3,42 +3,48 @@
 
 int main (int argc, char** argv)
 {
-	Cliente cliente;
-	while (true)
-	{
-		std::cout << "1. Guardar registro." << std::endl;
-		std::cout << "2. Consultar." << std::endl;
-		std::cout << "3. Salir." << std::endl;
-		char opcion;
-		std::cin >> opcion;
-		switch(opcion)
+	try
 		{
-			case '1':
-			case '2':
+		Cliente cliente;
+		while (true)
+		{
+			std::cout << "1. Guardar registro." << std::endl;
+			std::cout << "2. Consultar." << std::endl;
+			std::cout << "3. Salir." << std::endl;
+			char opcion;
+			std::cin >> opcion;
+			switch(opcion)
 			{
-				std::string nombre;
-				std::string dir;
-				std::string tel;
-				std::cout << "Ingrese nombre (o *):" << std::endl;
-				std::cin.ignore();
-				std::getline(std::cin, nombre);
-				std::cout << "Ingrese direccion (o *):" << std::endl;
-				std::getline(std::cin, dir);
-				std::cout << "Ingrese telefono (o *):" << std::endl;
-				std::getline(std::cin, tel);
-				std::cout << "ingreso:" << nombre << " - " << dir << " - " << tel << std::endl;
-				if (opcion == '1')
-						cliente.escribirEnBaseDeDatos(nombre, dir, tel);
-				if (opcion == '2')
-						cliente.consultarBaseDeDatos(nombre, dir, tel);
-				break;
+				case '1':
+				case '2':
+				{
+					std::string nombre;
+					std::string dir;
+					std::string tel;
+					std::cout << "Ingrese nombre (o *):" << std::endl;
+					std::cin.ignore();
+					std::getline(std::cin, nombre);
+					std::cout << "Ingrese direccion (o *):" << std::endl;
+					std::getline(std::cin, dir);
+					std::cout << "Ingrese telefono (o *):" << std::endl;
+					std::getline(std::cin, tel);
+					if (opcion == '1')
+							cliente.escribirEnBaseDeDatos(nombre, dir, tel);
+					if (opcion == '2')
+							cliente.consultarBaseDeDatos(nombre, dir, tel);
+					break;
+				}
+				case '3':
+					return 0;
+					break;
 			}
-			case '3':
-				return 0;
-				break;
 		}
+		return 0;
 	}
-	return 0;
+	catch (std::exception &e)
+	{
+		std::cout << "Cliente " << ::getpid() << " : " << e.what() << std::endl;
+	}
 }
 
 
